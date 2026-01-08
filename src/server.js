4f6099afdc2692ef356cf86ce13f89f936d37991
JooCrepaldi <joaopedrocrepaldi05@gmail.com>
2026-01-08 10:46:41 -0300

    Last commit because everything works!!

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { auth } from '../middlewares/auth.js';
 import functions from './functions.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
